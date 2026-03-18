13a8c38b0fcaf1d59da936651f51d425fcb7395d
olanrewajuprogress39-cpu <olanrewajuprogress39@gmail.com>
2026-03-18 10:55:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,33 +18,29 @@
  /* ══════════════════════════════════════════════
       PORTFOLIO FILTER
    ══════════════════════════════════════════════ */
-   
-   const veiwdesignicon = document.querySelector('#viewdesign');
-   const viewdesign = document.querySelector('.project-img');
-   const closedesign = document.querySelector('.cross');
+
 
    
 
-   veiwdesignicon.addEventListener('click', ()=>{
-    viewdesign.style.display = 'flex';
+   const viewdesign = document.querySelector('#viewdesign')
+   const viewdesign2 = document.querySelector('#viewdesign-2')
+   const projectcontainer = document.querySelector('.project-container');
+   const projectcontainer2 = document.querySelector('.project-container-2');
+   const cross = document.querySelector('.cross');
+   const cross2 = document.querySelector('.cross2');
+
+   viewdesign.addEventListener('click', function(){
+    projectcontainer.style.display = 'flex';
+   });
+   cross.addEventListener('click', function(){
+    projectcontainer.style.display = 'none';
    });
 
-   closedesign.addEventListener('click', ()=>{
-    viewdesign.style.display = 'none';
+   viewdesign2.addEventListener('click', function(){
+    projectcontainer2.style.display = 'flex';
    });
-
-   const viewdesignicon2 = document.querySelector('#viewdesign-2');
-   const viewdesign2 = document.querySelector('.project-img-2');
-   const closedesign2 = document.querySelector('.cross2');
-
-
-   viewdesignicon2.addEventListener('click', ()=>{
-    viewdesign2.style.display = 'flex';
+   cross2.addEventListener('click', function(){
+    projectcontainer2.style.display = 'none';
    });
-
-   closedesign2.addEventListener('click', ()=>{
-    viewdesign2.style.display = 'none';
-   });
-
 
   
